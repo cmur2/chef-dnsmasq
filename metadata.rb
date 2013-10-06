aed901a1bbb56222b9a44591726f9719a69fb8ac
cn <cn@mycrobase.de>
2013-10-06 21:45:07 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
 description      "Installs and configures dnsmasq in a very flexbile manner."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 
 supports "debian"
