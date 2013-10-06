c02795e1ae02b7ff1ad2ab7c07a58194a9bec6cd
cn <cn@mycrobase.de>
2013-10-06 21:14:34 +0200

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,8 @@ name             "dnsmasq"
 maintainer       "Christian Nicolai"
 maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
-description      ""
+description      "Installs and configures dnsmasq in a very flexbile manner."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
-#depends ""
-
-#suggests ""
-
-#supports ""
+supports "debian"
